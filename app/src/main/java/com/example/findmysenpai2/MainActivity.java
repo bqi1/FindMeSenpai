34852f33770939c813d1d656e9c83c8eaeb9c2af
brian <brian.qi138@gmail.com>
2021-09-18 20:51:10 -0600

    add and delete users

--- app/src/main/java/com/example/findmysenpai2/MainActivity.java
+++ app/src/main/java/com/example/findmysenpai2/MainActivity.java
@@ -8,10 +8,15 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.example.findmysenpai2.Senpai;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -26,14 +31,15 @@ public class MainActivity extends AppCompatActivity {
 //        Intent switchToMapActivity = new Intent(this, MapActivity.class);
 //        this.startActivity(switchToMapActivity);
         addUser("ABC123", "Weiwei");
+//        removeUser("ABC123", "Weiwei");
 
 
     }
     public void addUser(String roomcode, String name){
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         Senpai user = new Senpai();
-        user.setName(name);
-        user.setRoomCode(roomcode);
+        user.name = name;
+        user.roomCode = roomcode;
         db.collection("users")
                 .add(user)
                 .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
@@ -49,6 +55,37 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
+    }
+    public void removeUser(String roomcode, String name){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("users")
+                .whereEqualTo("name", name).whereEqualTo("roomCode",roomcode)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("TESTINGPASTA", document.getId() + " => " + document.getData());
+                                document.getReference().delete()
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Log.d("TESTINGPASTA", "DocumentSnapshot successfully deleted!");
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Log.w("TESTINGPASTA", "Error deleting document", e);
+                                            }
+                                        });
+                            }
+                        } else {
+                            Log.d("TESTINGPASTA", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
     }
 
 }
--- app/src/main/java/com/example/findmysenpai2/Senpai.java
+++ app/src/main/java/com/example/findmysenpai2/Senpai.java
@@ -4,25 +4,13 @@ package com.example.findmysenpai2;
 import java.util.UUID;
 // Have a firebase connection
 public class Senpai {
-    private String roomCode = "";
-    private String name = "";
-    public void setRoomCode(String roomCode){
-        this.roomCode = roomCode;
-    }
-    public String getRoomCode(){
-        return this.roomCode;
-    }
+    public String roomCode = "";
+    public String name = "";
     public void setPicture(){
 
     }
     public void getPicture(){
 
-    }
-    public String getName(){
-        return this.name;
-    }
-    public void setName(String name){
-        this.name = name;
     }
 
 }
