19d69c4d376ed91aa6d864e3ec3f7d024e69f6a9
WillQi <williamqipizza@gmail.com>
2021-09-18 20:31:10 -0600

    fix merge

--- app/src/main/java/com/example/findmysenpai2/MainActivity.java
+++ app/src/main/java/com/example/findmysenpai2/MainActivity.java
@@ -1,19 +1,54 @@
 package com.example.findmysenpai2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.findmysenpai2.Senpai;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("TESTINGPASTA", "KILLLME");
         this.setContentView(R.layout.activity_main);
+//        Intent switchToMapActivity = new Intent(this, MapActivity.class);
+//        this.startActivity(switchToMapActivity);
+        addUser("ABC123", "Weiwei");
 
-        Intent switchToMapActivity = new Intent(this, MapActivity.class);
-        this.startActivity(switchToMapActivity);
+
+    }
+    public void addUser(String roomcode, String name){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Senpai user = new Senpai();
+        user.setName(name);
+        user.setRoomCode(roomcode);
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("TESTINGPASTA", "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("TESTINGPASTA", "Error adding document", e);
+                    }
+                });
+
     }
 
 }
